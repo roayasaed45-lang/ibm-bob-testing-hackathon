1858851e12f5ee1e611436865e864c10381db172
roaya saed <roayasaed@MacBook-Pro-sl-roaya.local>
2026-08-31 18:40:52 +0300

    Set up Capacitor for iOS and Android

---
+++ android/app/src/main/java/com/alebarber/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.alebarber.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.alebarber.app',
+  appName: 'Ale Barber',
+  webDir: 'dist'
+};
+
+export default config;
